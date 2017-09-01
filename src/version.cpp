1dcdd992794263f597f1a34b7c722e503dbdadee
bumbacoin <bumbacoin@gmail.com>
2017-09-01 12:03:25 +0800

    Update version.cpp

--- src/version.cpp
+++ src/version.cpp
@@ -11,7 +11,7 @@
 const std::string CLIENT_NAME("Satoshi");
 
 // Client version number
-#define CLIENT_VERSION_SUFFIX   "-POS-test"
+#define CLIENT_VERSION_SUFFIX   "-POS"
 
 
 // The following part of the code determines the CLIENT_BUILD variable.
